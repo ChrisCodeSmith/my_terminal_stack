689d4b21bddcad5c061472d6ec5031d5a6321fc8
ChrisCodeSmith <chris.oversmith@gmail.com>
2023-05-07 11:01:13 +0200

    refactor pull script

--- pull_confs.sh
+++ pull_confs.sh
@@ -2,6 +2,11 @@
 REPO_NAME="my_terminal_stack"
 REPO_PATH="$HOME/repos/$REPO_NAME/"
 
-cp $HOME/.zshrc $REPO_PATH
-cp $HOME/.vimrc $REPO_PATH
-cp $HOME/.config/tmux/tmux.conf $REPO_PATH
+# Add file names here
+files=( .zshrc .vimrc .config/tmux/tmux.conf )
+
+# Loop to copy the relvant conf fils into the repo
+for file in "${files[@]}"
+do
+	cp $HOME/$file $REPO_PATH
+done
